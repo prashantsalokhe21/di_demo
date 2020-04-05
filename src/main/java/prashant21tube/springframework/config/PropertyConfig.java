d38b76538d10b8d421bcd8aa9c6c558deb1088b3
Prashant Salokhe <p.salokhe21@gmail.com>
2020-04-05 23:57:30 +0530

    Added fake JMS property file

--- src/main/java/prashant21tube/springframework/config/PropertyConfig.java
+++ src/main/java/prashant21tube/springframework/config/PropertyConfig.java
@@ -1,16 +1,27 @@
 package prashant21tube.springframework.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 import prashant21tube.springframework.propertybeans.FakeDataSource;
+import prashant21tube.springframework.propertybeans.FakeJmsBroker;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+//@PropertySource({"classpath:datasource.properties","classpath:jms.properties"})
+@PropertySources({
+	@PropertySource("classpath:datasource.properties"),
+	@PropertySource("classpath:jms.properties")
+})
 public class PropertyConfig {
+	
+	@Autowired
+	Environment env;
 	
 	@Value("${sqldb.username}")
 	String username;
@@ -20,14 +31,36 @@ public class PropertyConfig {
 	
 	@Value("${sqldb.url}")
 	String url;
- 
+	
+	@Value("${jms.username}")
+	String jmsUsername;
+	
+	@Value("${jms.password}")
+	String jmspassword;
+	
+	@Value("${jms.url}")
+	String jmsUrl;
+	 
 	@Bean
 	public FakeDataSource fakeDataSource() {
 		FakeDataSource dataSource = new FakeDataSource();
-		dataSource.setUsername(username);
+		//dataSource.setUsername(username);
+		/**
+		 * Use Environment variable which is set in Environment
+		 */
+		dataSource.setUsername(env.getProperty("USERNAME"));
 		dataSource.setPassword(password);
 		dataSource.setUrl(url);
 		return dataSource;
+	}
+	
+	@Bean
+	public FakeJmsBroker FakeJmsBroker() {
+		FakeJmsBroker broker = new FakeJmsBroker();
+		broker.setUsername(jmsUsername);
+		broker.setPassword(jmspassword);
+		broker.setUrl(jmsUrl);
+		return broker;		
 	}
 	
 	@Bean
